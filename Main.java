4cfd8a82d7670f43901de321db2e93ce12dd8415
reza <reza.khani1998@gmail.com>
2019-05-30 22:57:33 +0430

    add method

--- Main.java
+++ Main.java
@@ -3,6 +3,10 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     }
+
+    public static void tf(){
+        System.out.println("dg");
+    }
     public static void re(){
         System.out.println("df");
     }
