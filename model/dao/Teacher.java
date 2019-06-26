1fc99fe2df32948a4b381e8ff9df6533fbdf2b2a
reza <reza.khani1998@gmail.com>
2019-06-26 23:43:11 +0430

    add class Teacher

---
+++ model/dao/Teacher.java
@@ -0,0 +1,108 @@
+package HomeWork9.model.dao;
+
+import javax.persistence.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Entity
+@Table(name = "teachers")
+public class Teacher {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "teacher_code", nullable = false, unique = true)
+    private String teacherCode;
+
+    @Column(name = "salary", nullable = false)
+    private double salary;
+
+    @Column(name = "birth_day")
+    private LocalDate birthDay;
+
+
+    public Teacher(String firstName, String lastName, String teacherCode, double salary, String birthDay) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.teacherCode = teacherCode;
+        this.salary = salary;
+        this.birthDay = LocalDate.parse(birthDay);
+    }
+
+    public Teacher() {
+    }
+
+    
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTeacherCode() {
+        return teacherCode;
+    }
+
+    public void setTeacherCode(String teacherCode) {
+        this.teacherCode = teacherCode;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public LocalDate getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(LocalDate birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    // Override toString
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", teacherCode='" + teacherCode + '\'' +
+                ", salary=" + salary +
+                ", birthDay=" + birthDay +
+                '}';
+    }
+}
