09f0f56ccda1096db33f1ee2b59613448d7ad5e3
reza <reza.khani1998@gmail.com>
2019-06-26 23:21:01 +0430

    add property

--- model/dao/Student.java
+++ model/dao/Student.java
@@ -1,4 +1,18 @@
 package HomeWork9.model.dao;
+import javax.persistence.*;
 
+@Entity
+@Table(name = "students")
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+
 }
