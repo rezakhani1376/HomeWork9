cefe0754548f9f87c0facdc3299dc8457f8cca0d
reza <reza.khani1998@gmail.com>
2019-06-27 00:20:42 +0430

    modified Main

--- Main.java
+++ Main.java
@@ -1,13 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
-
-    public static void tf(){
-        System.out.println("dg");
-    }
-    public static void re(){
-        System.out.println("df");
+        // write your code here
+        System.out.println("HEllO");
     }
 }
